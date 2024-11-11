2825c78832845c0b19b6841392ecc5cb34fcbddc
NghiNV <nghinv03@gmail.com>
2024-11-11 11:00:52 +0700

    Admin template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,11 +6,12 @@ import { NzLayoutModule } from 'ng-zorro-antd/layout';
 import { NzMenuModule } from 'ng-zorro-antd/menu';
 import {HeaderComponent} from "../layout/header/header.component";
 import {FooterComponent} from "../layout/footer/footer.component";
+import {SearchComponent} from "../views/search/search.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterLink, RouterOutlet, NzIconModule, NzLayoutModule, NzMenuModule, HeaderComponent, FooterComponent],
+  imports: [CommonModule, RouterLink, RouterOutlet, NzIconModule, NzLayoutModule, NzMenuModule, HeaderComponent, FooterComponent, SearchComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
--- src/views/search/search.component.ts
+++ src/views/search/search.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
+import {FormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-search',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule
+  ],
   templateUrl: './search.component.html',
   styleUrl: './search.component.scss'
 })
 export class SearchComponent {
+  @Input() isSearchVisible = true;
+  @Input() searchQuery = "";
 
+  onSearch() {
+
+  }
+
+  closeSearch() {
+    this.isSearchVisible = false;
+  }
+
+  clearSearch() {
+
+  }
 }
