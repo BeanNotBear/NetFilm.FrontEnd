74d1d446c42dc2f7bf3676e986c37b6e125f6fa4
NghiNV <nghinv03@gmail.com>
2024-11-13 20:28:20 +0700

    Dashboard component

---
+++ src/views/dashboard/dashboard.component.ts
@@ -0,0 +1,27 @@
+import {Component} from '@angular/core';
+import {NzContentComponent, NzHeaderComponent, NzLayoutComponent, NzSiderComponent} from "ng-zorro-antd/layout";
+import {NzIconDirective} from "ng-zorro-antd/icon";
+import {NzMenuDirective, NzMenuItemComponent, NzSubMenuComponent} from "ng-zorro-antd/menu";
+import {RouterLink, RouterOutlet} from "@angular/router";
+
+@Component({
+  selector: 'app-dashboard',
+  standalone: true,
+  imports: [
+    NzContentComponent,
+    NzHeaderComponent,
+    NzIconDirective,
+    NzLayoutComponent,
+    NzMenuDirective,
+    NzMenuItemComponent,
+    NzSiderComponent,
+    NzSubMenuComponent,
+    RouterLink,
+    RouterOutlet
+  ],
+  templateUrl: './dashboard.component.html',
+  styleUrl: './dashboard.component.scss'
+})
+export class DashboardComponent {
+  isCollapsed = false;
+}
