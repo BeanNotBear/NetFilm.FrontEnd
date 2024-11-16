ca2eaca91c3a7358f8e7d83e3edb2d771bbc421e
NghiNV <nghinv03@gmail.com>
2024-11-17 00:20:39 +0700

    movie list almost done

--- src/views/movie-list/movie-list.component.ts
+++ src/views/movie-list/movie-list.component.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {MovieAreaComponent} from "../movie-area/movie-area.component";
+import {MovieService} from "../../service/movie.service";
+import {MovieViewerDto} from "../../models/movieDtos/movie.viewer.dto";
+import {MovieParam} from "../../models/movieDtos/movie.param";
 
 @Component({
   selector: 'app-movie-list',
@@ -10,6 +13,43 @@ import {MovieAreaComponent} from "../movie-area/movie-area.component";
   templateUrl: './movie-list.component.html',
   styleUrl: './movie-list.component.scss'
 })
-export class MovieListComponent {
+export class MovieListComponent implements OnInit{
+  movies: MovieViewerDto[] = [];
+  constructor(private movieService: MovieService) {
+
+  }
+
+  ngOnInit(): void {
+    this.fetchMovies();
+  }
+
+  fetchMovies() {
+    const movieParam = new MovieParam(
+      1,
+      12,
+      "",
+      null,
+      null,
+      null,
+      null,
+      null,
+      null,
+      null,
+      null,
+      false,
+      null,
+      null,
+      null
+    );
+    this.movieService.getMoviesViewer(movieParam).subscribe({
+      next: data => {
+        this.movies = data.items;
+      },
+      error: err => {
+
+      }
+    });
+  }
+
 
 }
